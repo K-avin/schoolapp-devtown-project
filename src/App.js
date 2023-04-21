28dfbb350f9a7fdedac060832ced2db4c305ce6d
k-vin <kabilraj050@gmail.com>
2023-04-21 10:04:18 +0530

    data update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './common/navBar';
 import Footer from './common/footer';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('./data/students.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error));
+  }, []);
   return (
     <div className="App">
       <NavBar />
+      <div class="row">
+        <div class="col-md-3">
+          {data.map(user => (
+            <a>{user}</a>
+          ))}
+        </div>
+      </div>
       <Footer />
     </div>
   );
---
+++ src/components/studentCart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const StudentCart = ({ data }) => {
+    return (
+        <div class="card text-center">
+            <div class="card-body">
+                <img src="profile1.jpg" class="rounded-circle mb-3" alt="Profile Image" width="100" height="100" />
+                <h5 class="card-title">John Smith</h5>
+                <p class="card-text">10th Grade</p>
+            </div>
+        </div>
+    )
+}
+
+export default StudentCart
